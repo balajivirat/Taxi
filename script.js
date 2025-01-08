a3978d0a28fd5b6c89de98f9377415e66e8ab601
balajivirat <balajikvirat@gmail.com>
2025-01-08 16:33:25 +0530

    alert removed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("taxiForm").addEventListener("submit", async function (event) {
     event.preventDefault(); // Prevent form from refreshing the page
-  alert('Booking Confirmed')
+  // alert('Booking Confirmed')
     const pickup = document.getElementById("pickup").value;
     const dropoff = document.getElementById("dropoff").value;
     const date = document.getElementById("date").value;
@@ -25,7 +25,7 @@ document.getElementById("taxiForm").addEventListener("submit", async function (e
     document.getElementById("confirmation").innerHTML = confirmationMessage;
     
     // JavaScript Fetch Example
-    const url = 'http://localhost:3000/send-email';
+    const url = 'https://taximailer.onrender.com/send-email';
 
 
 const data = {
